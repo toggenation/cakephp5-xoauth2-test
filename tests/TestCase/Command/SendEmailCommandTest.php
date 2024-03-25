15677551538731378dbc480e5897bc4dd58eb9ca
James McDonald <james@toggen.com.au>
2024-03-26 10:01:00 +1100

    Initial commit

---
+++ tests/TestCase/Command/SendEmailCommandTest.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\TestCase\Command;
+
+use App\Command\SendEmailCommand;
+use Cake\Console\TestSuite\ConsoleIntegrationTestTrait;
+use Cake\TestSuite\TestCase;
+
+/**
+ * App\Command\SendEmailCommand Test Case
+ *
+ * @uses \App\Command\SendEmailCommand
+ */
+class SendEmailCommandTest extends TestCase
+{
+    use ConsoleIntegrationTestTrait;
+
+    /**
+     * Test buildOptionParser method
+     *
+     * @return void
+     * @uses \App\Command\SendEmailCommand::buildOptionParser()
+     */
+    public function testBuildOptionParser(): void
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test execute method
+     *
+     * @return void
+     * @uses \App\Command\SendEmailCommand::execute()
+     */
+    public function testExecute(): void
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
